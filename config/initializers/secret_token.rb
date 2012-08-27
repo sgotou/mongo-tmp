20e9ac73d906920bc3f8693f658867b47af9c3de
sgotou <sgotou@crooz.co.jp>
2012-08-27 12:30:10 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hai::Application.config.secret_token = 'd7788018c79eabf379de0908ed52bd1bbce416d29c1bff6dc6f7baafeea734ad2f9040f52afe78262257f71cacea323775719b6ada099afef16056171e0d0aed'
